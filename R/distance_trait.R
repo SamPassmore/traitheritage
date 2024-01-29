26e7bd594b94c5e5f4a92f0be4fcc08589e30215
Sam Passmore <passmore.sam@gmail.com>
2024-01-29 19:17:59 +1100

    add distance function & reshape2

---
+++ R/distance_trait.R
@@ -0,0 +1,7 @@
+## Distance function
+
+distance_trait_heritage = function(trees, trait, n_generations, distance_matrix, cut_off){
+
+
+
+}
--- R/posterior.R
+++ R/posterior.R
@@ -12,7 +12,10 @@
 
 posterior_trait_heritage = function(trees, trait, n_generations){
 
-  if(!"multiPhylo" %in% class(trees)) stop("Trees should be of class multiPhylo. If you only have one tree, use the trait_heritage function.")
+  if (!"multiPhylo" %in% class(trees))
+    stop(
+      "Trees should be of class multiPhylo. If you only have one tree, use the trait_heritage function."
+    )
 
   output = lapply(trees, function(t){
     trait_heritage(tree = t, trait = trait, n_generations = n_generations)
@@ -20,5 +23,4 @@ posterior_trait_heritage = function(trees, trait, n_generations){
   names(output) = 1:length(trees)
 
   dplyr::bind_rows(output, .id = "posterior")
-
 }
