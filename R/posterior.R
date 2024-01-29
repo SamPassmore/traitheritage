8f8a3821037feae2c09a2f6fd3bfbf6e455b40a2
Sam Passmore <passmore.sam@gmail.com>
2024-01-29 19:00:53 +1100

    add posterior documentation

--- R/posterior.R
+++ R/posterior.R
@@ -1,5 +1,15 @@
 ### Calculate the posterior probability of a tree
 
+#' Calculate the probability of trait heritage across a posterior sample of trees
+#'
+#' @param trees A posterior sample of trees, of class multiPhylo
+#' @param trait A named vector of trait values. Names should match taxa
+#' @param n_generations A numeric value indicating how many equal cuts to make in the tree
+#'
+#' @return a Dataframe showing the probability of a shared trait for each generation, witihn each tree
+#' @export
+#'
+
 posterior_trait_heritage = function(trees, trait, n_generations){
 
   if(!"multiPhylo" %in% class(trees)) stop("Trees should be of class multiPhylo. If you only have one tree, use the trait_heritage function.")
--- tests/testthat/test-posterior.R
+++ tests/testthat/test-posterior.R
@@ -14,5 +14,20 @@ test_that("#1. Simple posterior test", {
   result = posterior_trait_heritage(trees, trait, n_generations = 2)
 
   expect_equal(result$clade_probability, c(1, 1, 1, 1))
+})
 
+test_that("#1. Second simple posterior test", {
+
+  t = ape::read.tree(text = "((A,B),(C,D));")
+  t = ape::compute.brlen(t)
+
+  trees = list(t, t, t, t)
+  class(trees) = "multiPhylo"
+
+  trait = c("a", "b", "b", "b")
+  names(trait) = c("A", "B", "C", "D")
+
+  result = posterior_trait_heritage(trees, trait, n_generations = 2)
+
+  expect_equal(result$clade_probability, c(0.5, 0.5, 0.5, 0.5))
 })
