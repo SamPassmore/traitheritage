2cb8f2989b41645a33ef0f7a63cbefb3b66295f7
Sam Passmore <passmore.sam@gmail.com>
2025-07-09 15:08:39 +1000

    minor code cleaning

--- R/trait_heritage.R
+++ R/trait_heritage.R
@@ -2,40 +2,43 @@
 #'
 #' @param tree a single phylogeny
 #' @param trait a vector of traits, named for each taxa
-#' @param generation_time the number of times to cut a tree and calculate the probability of trait heritage
+#' @param generation_time how frequently the trait probability should be calculated. Must be less than the height of the phylogeny.
 #'
-#' @return a dataframe containing the probability of shared languages within each generation
+#' @return a list containing two dataframes. by_trait contains the probability calculation at each timestep for each level of the trait. summary shows the probability across trait values for each time step.
 #' @export
 #'
 trait_heritage = function(tree, trait, generation_time){
 
+  ## Tests
   if(any(is.na(trait))) stop("No NA trait values are allowed.")
   if(is.null(names(trait))) stop("trait must have names that match the taxa. Ensure trait is a named vector.")
   if(!all(names(trait) %in% tree$tip.label)) stop("Some tips have no matching trait. Make sure all tips have a trait.")
 
+  ### First identify the hierachical relationship of the tree
   # clades sets at each node
   descendants = phangorn::Descendants(tree)
   names(descendants) = seq_along(descendants)
-  # clades with more than one taxa
+
+  # Deal with clades that contain more than one taxa
   desc_multi = descendants[sapply(descendants, length) > 1]
   desc_pairs = lapply(desc_multi, function(x) data.table(t(combn(x, 2))))
   dp_df = data.table::rbindlist(desc_pairs, idcol= "node")
 
-  # Remove duplicated pairs in the phylogenetic hierarchy
+  # Remove duplicated pairs in the phylogenetic hierarchy (i.e. reversed)
   dp_df = dp_df[!duplicated(dp_df, by = c("V1", "V2"), fromLast = TRUE)]
 
-  # make a reference table for taxa id to speed up taxa matching
+  # make a reference table of taxa id to speed up taxa matching
   ref = data.table(taxa = tree$tip.label, ind = as.numeric(as.factor(tree$tip.label)))
   trait_dt = data.table(taxa = names(trait), trait = trait)
   ref = ref[trait_dt, on = "taxa"]
 
-  # merge in trait data
+  # merge reference table into the trait data
   dp_df = merge.data.table(dp_df, ref, by.x = "V1", by.y = "ind", all.x = TRUE)
   dp_df = merge.data.table(dp_df, ref, by.x = "V2", by.y = "ind", all.x = TRUE)
 
   # identify shared traits
   dp_df[, trait := trait.x == trait.y]
-  dp_df[, trait_named := ifelse(trait.x == trait.y, trait.x, "DIFFERENT")]
+  dp_df[, trait_named := ifelse(trait.x == trait.y, trait.x, "DIFF")]
 
   # Identify which clades are under a certain time point
   # allows for non-ultrametric trees
