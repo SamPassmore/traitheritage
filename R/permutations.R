67322219b516ef19ddda8c445d2298f82abd08a9
Sam Passmore <passmore.sam@gmail.com>
2025-08-27 16:20:49 +1000

    trait_heritage _specific permutations added

--- R/permutations.R
+++ R/permutations.R
@@ -28,3 +28,24 @@ permute_trait_heritage = function(tree, trait, generation_time, n_permutations =
 
   return(list(by_trait = permuted_bytrait, summary = permuted_summary))
 }
+
+permute_trait_heritage_specific = function(tree, trait, generation_time, n_permutations = 100, condition = NULL, seed = 937){
+  set.seed(seed)
+  # Permuted results
+  permuted_list = lapply(1:n_permutations, function(i){
+    p_trait = trait
+    names(p_trait) = names(p_trait)[sample(1:length(p_trait))]
+    trait_heritage_specific(tree, p_trait, generation_time, condition = condition)
+  })
+
+  by_trait = lapply(permuted_list, "[[", 1)
+  p_summary = lapply(permuted_list, "[[", 2)
+
+  names(by_trait) = paste0("p_", 1:n_permutations)
+  names(p_summary) = paste0("p_", 1:n_permutations)
+
+  permuted_bytrait = data.table::rbindlist(by_trait, idcol = "iteration")
+  permuted_summary = data.table::rbindlist(p_summary, idcol = "iteration")
+
+  return(list(by_trait = permuted_bytrait, summary = permuted_summary))
+}
